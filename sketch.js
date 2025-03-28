de3712069c4848f35b847232e9cafcbf26dbcb66
tobywinter64 <9247@shenfield.essex.sch.uk>
2025-03-28 14:44:53 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const csvInput = document.getElementById('csvInput');
-    const randomArray = Array.from({ length: 25000 }, () => Math.floor(Math.random() * 100) + 1);
-    csvInput.value = randomArray.join(',');
 
-    document.getElementById('sortButton').addEventListener('click', function() {
-        const dataArray = csvInput.value.split(',').map(Number);
+document.addEventListener('DOMContentLoaded', function() {
+    const arrayInput = document.getElementById('array'); // Changed from 'csvInput'
+    const randomArray = Array.from({ length: 25000 }, () => Math.floor(Math.random() * 100) + 1);
+    arrayInput.value = randomArray.join(',');
+
+    document.getElementById('sortButton').addEventListener('click', function() { // ID for button added in index.html
+        const dataArray = arrayInput.value.split(',').map(Number);
         const startTime = performance.now();
-        
+
         bubbleSort(dataArray);
-        
+
         const endTime = performance.now();
         const timeTaken = endTime - startTime;
         document.getElementById('timer').innerText = `Time: ${timeTaken.toFixed(10)}ms`;
-        document.getElementById('sortedOutput').value = dataArray.join(',');
+        document.getElementById('output').value = dataArray.join(','); // Changed from 'sortedOutput'
     });
 });
